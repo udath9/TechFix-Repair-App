56af00220b228d2a0ac543f4589f51b86117c7e7
udath <udathprabhashwara@gmail.com>
2026-08-28 16:47:44 +0530

    Created customer home page and BookRepair Activity

---
+++ app/src/main/java/com/up9/techfix/booking/BookRepairActivity.java
@@ -0,0 +1,26 @@
+package com.up9.techfix.booking;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.up9.techfix.R;
+
+public class BookRepairActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_book_repair);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
--- app/src/main/java/com/up9/techfix/customer/CustomerHomeActivity.java
+++ app/src/main/java/com/up9/techfix/customer/CustomerHomeActivity.java
@@ -1,26 +1,59 @@
 package com.up9.techfix.customer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.up9.techfix.R;
+import com.up9.techfix.payment.PaymentActivity;
+import com.up9.techfix.service.ServicesActivity;
 
 public class CustomerHomeActivity extends AppCompatActivity {
+
+    private Button btnServices;
+    private Button btnBookRepair;
+    private Button btnTrackRepair;
+    private Button btnHistory;
+    private Button btnPayment;
+    private Button btnBranches;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_customer_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnServices = findViewById(R.id.btnServices);
+        btnBookRepair = findViewById(R.id.btnBookRepair);
+        btnTrackRepair = findViewById(R.id.btnTrackRepair);
+        btnHistory = findViewById(R.id.btnHistory);
+        btnPayment = findViewById(R.id.btnPayment);
+        btnBranches = findViewById(R.id.btnBranches);
+
+        btnServices.setOnClickListener(v -> {
+            Intent intent = new Intent(CustomerHomeActivity.this, ServicesActivity.class);
+            startActivity(intent);
+        });
+
+        btnTrackRepair.setOnClickListener(v -> {
+            Intent intent = new Intent(CustomerHomeActivity.this, RepairTrackingActivity.class);
+            startActivity(intent);
+        });
+
+        btnHistory.setOnClickListener(v -> {
+            Intent intent = new Intent(CustomerHomeActivity.this, RepairHistoryActivity.class);
+            startActivity(intent);
+        });
+
+        btnPayment.setOnClickListener(v -> {
+            Intent intent = new Intent(CustomerHomeActivity.this, PaymentActivity.class);
+            startActivity(intent);
+        });
+
+        btnBranches.setOnClickListener(v -> {
+            Intent intent = new Intent(CustomerHomeActivity.this, BranchesActivity.class);
+            startActivity(intent);
         });
     }
 }
