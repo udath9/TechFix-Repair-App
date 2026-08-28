fd008fd7f3028b13ab9618f35147b468881e03cb
udath <udathprabhashwara@gmail.com>
2026-08-28 16:06:33 +0530

    created login screen

--- app/src/main/java/com/up9/techfix/auth/LoginActivity.java
+++ app/src/main/java/com/up9/techfix/auth/LoginActivity.java
@@ -1,26 +1,71 @@
 package com.up9.techfix.auth;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.up9.techfix.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText etEmail;
+    private EditText etPassword;
+    private Button btnLogin;
+    private TextView tvRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        tvRegister = findViewById(R.id.tvRegister);
+
+        btnLogin.setOnClickListener(v -> {
+            validateLogin();
         });
+
+        tvRegister.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    private void validateLogin() {
+
+        String email = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString();
+
+        if (email.isEmpty()) {
+            etEmail.setError("Please enter your email");
+            etEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            etEmail.setError("Please enter a valid email");
+            etEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            etPassword.setError("Please enter your password");
+            etPassword.requestFocus();
+            return;
+        }
+
+        Toast.makeText(
+                this,
+                "Login details are valid!",
+                Toast.LENGTH_SHORT
+        ).show();
     }
 }
