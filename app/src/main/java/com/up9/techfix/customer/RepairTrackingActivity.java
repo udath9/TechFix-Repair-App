0abf2601b711a9ff5654c8dfb1c625003d4b7974
udath <udathprabhashwara@gmail.com>
2026-08-30 05:44:33 +0530

    Added repair tracking screen with status timeline

--- app/src/main/java/com/up9/techfix/customer/RepairTrackingActivity.java
+++ app/src/main/java/com/up9/techfix/customer/RepairTrackingActivity.java
@@ -17,10 +17,24 @@ public class RepairTrackingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_repair_tracking);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(
+                findViewById(R.id.main),
+                (v, insets) -> {
+
+                    Insets systemBars = insets.getInsets(
+                            WindowInsetsCompat.Type.systemBars()
+                    );
+
+                    v.setPadding(
+                            systemBars.left,
+                            systemBars.top,
+                            systemBars.right,
+                            systemBars.bottom
+                    );
+
+                    return insets;
+                }
+        );
     }
 }
