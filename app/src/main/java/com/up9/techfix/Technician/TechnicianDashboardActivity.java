864d7dd393113651f8b6086240795f6f94e7c903
Pasindu Malith <wijesinghamalith99@gmail.com>
2026-08-30 21:19:36 +0530

    Add TechnicianDashboardActivity and layout

---
+++ app/src/main/java/com/up9/techfix/Technician/TechnicianDashboardActivity.java
@@ -0,0 +1,22 @@
+package com.up9.techfix.Technician;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.up9.techfix.R;
+
+public class TechnicianDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_technician_dashboard);
+
+    }
+}
